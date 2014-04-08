85f9aba63cda5c0d239a8027885cf56be83ef425
Chris Knepper <cxk1108@rit.edu>
2014-04-08 19:11:50 -0400

    -Put code to read temperature in a function -Read temperature on every request to /json

--- in-glass.py
+++ in-glass.py
@@ -2,17 +2,6 @@ from flask import Flask, url_for, render_template, jsonify
 from temperusb import TemperHandler
 
 app = Flask(__name__)
-
-#Get temperature from temper-python
-th = TemperHandler()
-devs = th.get_devices()
-readings = [] #Building block of an API
-for i, dev in enumerate(devs):
-    readings.append({'device': i,
-                     'c': dev.get_temperature(),
-                     'f': dev.get_temperature(format="fahrenheit"),
-                     })
-
 temp = 50 #Temporary dummy value for HTML display
 
 #Output temperature on an HTML page
@@ -23,7 +12,19 @@ def main_page():
 #Output temperature as a json object
 @app.route('/json')
 def json_output():
-    return jsonify(readings=readings)
+    return jsonify(readings=get_temperatures())
+
+def get_temperatures():
+	#Get temperature from temper-python
+	th = TemperHandler()
+	devs = th.get_devices()
+	readings = [] #Building block of an API
+	for i, dev in enumerate(devs):
+	    readings.append({'device': i,
+	                     'c': dev.get_temperature(),
+	                     'f': dev.get_temperature(format="fahrenheit"),
+	                     })
+	return readings
 
 #Only execute when called from the terminal
 if __name__ == '__main__':
